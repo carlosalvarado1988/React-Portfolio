68e3fea9f5070b49e8553c77b1ec98b25e2f48cd
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-08-15 17:13:41 -0600

    adding ThreeFiber page with components

---
+++ src/components/ThreeFiber/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+import { Environment } from './Environment';
+import { House } from './House';
+
+interface ThreeFiberProps {}
+
+export const ThreeFiber: React.FunctionComponent<ThreeFiberProps> = (props) => {
+  return (
+    <Canvas shadows camera={{ position: [-15, 10, 15], fov: 25 }}>
+      <color attach="background" args={['skyblue']} />
+      <House />
+      <Environment />
+      <OrbitControls makeDefault />
+    </Canvas>
+  );
+};
