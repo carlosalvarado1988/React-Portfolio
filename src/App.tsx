a3b5c52be8f5046c7ba9934492d0af0fc16995cf
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-08-15 17:58:24 -0600

    Adding styles components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import styled from 'styled-components';
 import { Home } from './components/Home';
 import { Demos } from './components/Demos';
 import { ThreeFiber } from './components/ThreeFiber';
 
 export interface AppProps {}
 
+const AppContainer = styled.div`
+    height: 100vh;
+`;
+
 const App: React.FunctionComponent<AppProps> = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="demos">
-                    <Route index element={<Demos />} />
-                    <Route path="images-explore" element={<ThreeFiber />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <AppContainer>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="demos">
+                        <Route index element={<Demos />} />
+                        <Route path="images-explore" element={<ThreeFiber />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </AppContainer>
     );
 };
 
--- src/components/ThreeFiber/index.tsx
+++ src/components/ThreeFiber/index.tsx
@@ -1,19 +1,25 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
 import { Environment } from './Environment';
 import { House } from './House';
 
-interface ThreeFiberProps {}
+const StyledCanvas = styled(Canvas)`
+  height: 800px;
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
 
-export const ThreeFiber: React.FunctionComponent<ThreeFiberProps> = (props) => {
+export const ThreeFiber: React.FunctionComponent = () => {
   return (
-    <Canvas shadows camera={{ position: [-15, 10, 15], fov: 25 }}>
+    <StyledCanvas shadows camera={{ position: [-15, 10, 15], fov: 25 }}>
       <color attach="background" args={['skyblue']} />
       <House />
       <Environment />
       <OrbitControls makeDefault />
-    </Canvas>
+    </StyledCanvas>
   );
 };
