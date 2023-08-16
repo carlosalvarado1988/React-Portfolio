618b99c9357996ef9e4a1b96917cf30ba1b54b82
Carlos Alvarado <c.alvarado_@hotmail.com>
2023-08-16 09:14:50 -0600

    adding navigation to Canvas page

--- src/components/ThreeFiber/index.styles.js
+++ src/components/ThreeFiber/index.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Canvas } from '@react-three/fiber';
 
 export const StyledCanvas = styled(Canvas)`
   height: 800px;
--- src/components/ThreeFiber/index.tsx
+++ src/components/ThreeFiber/index.tsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
-import { Canvas } from '@react-three/fiber';
+import { useNavigate } from 'react-router-dom';
 import { OrbitControls } from '@react-three/drei';
+import styled from 'styled-components';
+import { Box, Button } from '@mui/material';
 
 import { StyledCanvas } from './index.styles';
 import { Environment } from './Environment';
 import { House } from './House';
 
+const NavigationBox = styled(Box)`
+  display: flex;
+  position: absolute;
+  z-index: 1;
+  padding: 10px;
+`;
+
 export const ThreeFiber: React.FunctionComponent = () => {
+  const navigate = useNavigate();
   return (
-    <StyledCanvas shadows camera={{ position: [-15, 10, 15], fov: 25 }}>
-      <color attach="background" args={['skyblue']} />
-      <House />
-      <Environment />
-      <OrbitControls makeDefault />
-    </StyledCanvas>
+    <>
+      <NavigationBox>
+        <Button variant="contained" onClick={() => navigate('/#portfolio')}>
+          Home
+        </Button>
+      </NavigationBox>
+      <StyledCanvas shadows camera={{ position: [-15, 10, 15], fov: 25 }}>
+        <color attach="background" args={['skyblue']} />
+        <House />
+        <Environment />
+        <OrbitControls makeDefault />
+      </StyledCanvas>
+    </>
   );
 };
